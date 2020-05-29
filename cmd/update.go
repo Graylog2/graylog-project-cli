78babf0acc56b5b139073badfd252cf2af5fa180
Bernd Ahlers <bernd@graylog.com>
2020-05-29 18:16:26 +0200

    Also update the graylog-project repository itself

--- cmd/update.go
+++ cmd/update.go
@@ -2,11 +2,13 @@ package cmd
 
 import (
 	c "github.com/Graylog2/graylog-project-cli/config"
+	"github.com/Graylog2/graylog-project-cli/git"
 	"github.com/Graylog2/graylog-project-cli/logger"
 	"github.com/Graylog2/graylog-project-cli/manifest"
 	"github.com/Graylog2/graylog-project-cli/pom"
 	p "github.com/Graylog2/graylog-project-cli/project"
 	"github.com/Graylog2/graylog-project-cli/repo"
+	"github.com/Graylog2/graylog-project-cli/utils"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -41,6 +43,27 @@ func updateCommand(cmd *cobra.Command, args []string) {
 	manifestFiles := manifest.ReadState().Files()
 	repoMgr := repo.NewRepoManager(config)
 	project := p.New(config, manifestFiles)
+
+	projectRepoPath, err := git.ToplevelPath()
+	if err != nil {
+		logger.Fatal("Couldn't get top-level path: %v", err)
+	}
+
+	logger.Info("Updating %v", projectRepoPath)
+	utils.InDirectory(projectRepoPath, func() {
+		args := []string{"pull"}
+
+		if config.Update.Prune {
+			args = append(args, "--prune")
+		}
+		if config.Update.Relaxed {
+			args = append(args, "--ff")
+		} else {
+			args = append(args, "--ff-only")
+		}
+
+		git.Git(args...)
+	})
 
 	p.ForEachSelectedModule(project, func(module p.Module) {
 		logger.Info("Updating %v", module.Path)
