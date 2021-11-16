aac1e8d22a401bba3fc81994e2aed46e02182a2a
Marco Pfatschbacher <marco@graylog.com>
2021-11-16 14:55:35 +0100

    Print commit IDs with the status command (#29)
    That makes it easier to identify if a build contained a certain change or not.
    
    Co-authored-by: Bernd Ahlers <bernd@graylog.com>

--- cmd/status.go
+++ cmd/status.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"strconv"
 
-	"github.com/Graylog2/graylog-project-cli/config"
+	c "github.com/Graylog2/graylog-project-cli/config"
 	"github.com/Graylog2/graylog-project-cli/git"
 	"github.com/Graylog2/graylog-project-cli/logger"
 	"github.com/Graylog2/graylog-project-cli/manifest"
@@ -29,7 +29,7 @@ func init() {
 }
 
 func statusCommand(cmd *cobra.Command, args []string) {
-	config := config.Get()
+	config := c.Get()
 	manifestFiles := manifest.ReadState().Files()
 	project := p.New(config, manifestFiles)
 
@@ -46,6 +46,14 @@ func statusCommand(cmd *cobra.Command, args []string) {
 			continue
 		}
 		utils.InDirectory(module.Path, func() {
+			var commitId string
+			if c.IsCI() {
+				// Don't display shortened commit IDs in CI environments to avoid future ambiguities regarding the
+				// shortened commit IDs
+				commitId = git.GitValue("rev-parse", "HEAD")
+			} else  {
+				commitId = git.GitValue("rev-parse", "--short", "HEAD")
+			}
 			revision := git.GitValue("rev-parse", "--abbrev-ref", "HEAD")
 			gitStatus := git.GitValue("status", "--porcelain")
 			filesModified, filesDeleted, filesAdded := 0, 0, 0
@@ -64,19 +72,19 @@ func statusCommand(cmd *cobra.Command, args []string) {
 			}
 
 			if !module.HasParent() {
-				logger.Info("    %-"+strconv.Itoa(int(maxNameLength))+"s  %s (branch: %s)", module.Name, module.Version(), revision)
+				logger.Info("    %-"+strconv.Itoa(int(maxNameLength))+"s  %s (commit: %s, branch: %s)", module.Name, module.Version(), commitId, revision)
 				if config.Verbose {
 					logger.ColorInfo(color.FgYellow, "        <no parent>")
 				}
 			} else {
 				if config.Verbose {
-					logger.Info("    %-"+strconv.Itoa(int(maxNameLength))+"s  %s (branch: %s)", module.Name, module.Version(), revision)
+					logger.Info("    %-"+strconv.Itoa(int(maxNameLength))+"s  %s (commit: %s, branch: %s)", module.Name, module.Version(), commitId, revision)
 					logger.ColorInfo(color.FgYellow, "        Parent groupId:      %s", module.ParentGroupId())
 					logger.ColorInfo(color.FgYellow, "        Parent artifactId:   %s", module.ParentArtifactId())
 					logger.ColorInfo(color.FgYellow, "        Parent version:      %s", module.ParentVersion())
 					logger.ColorInfo(color.FgYellow, "        Parent relativePath: %s", module.ParentRelativePath())
 				} else {
-					logger.Info("    %-"+strconv.Itoa(int(maxNameLength))+"s  %s (branch: %s, parent: %s)", module.Name, module.Version(), revision, module.ParentVersion())
+					logger.Info("    %-"+strconv.Itoa(int(maxNameLength))+"s  %s (commit: %s, branch: %s, parent: %s)", module.Name, module.Version(), commitId, revision, module.ParentVersion())
 				}
 			}
 			if !config.Verbose && (filesModified > 0 || filesDeleted > 0 || filesAdded > 0) {
