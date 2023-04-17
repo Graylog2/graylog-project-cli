d323ca4dac8ccd3551cb2409fbaf4724b51e01c2
Bernd Ahlers <bernd@graylog.com>
2023-04-17 15:22:28 +0200

    Show error messages in quiet mode

--- logger/logger.go
+++ logger/logger.go
@@ -26,29 +26,35 @@ func SetPrefix(value string) {
 }
 
 func Info(format string, args ...interface{}) {
-	println(prefixColor, noColor, prefix, format, args...)
+	if !quiet {
+		println(prefixColor, noColor, prefix, format, args...)
+	}
 }
 
 func InfoWithPrefix(customPrefix string, format string, args ...interface{}) {
-	println(prefixColor, noColor, customPrefix, format, args...)
+	if !quiet {
+		println(prefixColor, noColor, customPrefix, format, args...)
+	}
 }
 
 func ColorInfo(colorValue color.Attribute, format string, args ...interface{}) {
-	println(prefixColor, colorValue, prefix, format, args...)
+	if !quiet {
+		println(prefixColor, colorValue, prefix, format, args...)
+	}
 }
 
 func Error(format string, args ...interface{}) {
-	ColorInfo(errorColor, format, args...)
+	println(prefixColor, errorColor, prefix, format, args...)
 }
 
 func Debug(format string, args ...interface{}) {
-	if debug {
+	if debug && !quiet {
 		Info(format, args...)
 	}
 }
 
 func DebugWithPrefix(customPrefix string, format string, args ...interface{}) {
-	if debug {
+	if debug && !quiet {
 		InfoWithPrefix(customPrefix, format, args...)
 	}
 }
@@ -59,19 +65,27 @@ func Fatal(format string, args ...interface{}) {
 }
 
 func Println(format string, args ...interface{}) {
-	println(noColor, noColor, "", format, args...)
+	if !quiet {
+		println(noColor, noColor, "", format, args...)
+	}
 }
 
 func Printf(format string, args ...interface{}) {
-	print(noColor, noColor, "", format, args...)
+	if !quiet {
+		print(noColor, noColor, "", format, args...)
+	}
 }
 
 func ColorPrintln(colorValue color.Attribute, format string, args ...interface{}) {
-	println(noColor, colorValue, "", format, args...)
+	if !quiet {
+		println(noColor, colorValue, "", format, args...)
+	}
 }
 
 func ColorPrintf(colorValue color.Attribute, format string, args ...interface{}) {
-	print(noColor, colorValue, "", format, args...)
+	if !quiet {
+		print(noColor, colorValue, "", format, args...)
+	}
 }
 
 func println(prefixColor color.Attribute, textColor color.Attribute, prefix string, format string, args ...interface{}) {
@@ -79,10 +93,6 @@ func println(prefixColor color.Attribute, textColor color.Attribute, prefix stri
 }
 
 func print(prefixColor color.Attribute, textColor color.Attribute, prefix string, format string, args ...interface{}) {
-	if quiet {
-		return
-	}
-
 	if prefix != "" {
 		color.New(prefixColor).Printf("%s ", prefix)
 	}
