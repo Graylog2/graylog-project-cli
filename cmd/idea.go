3ea4e08b032c566019de91e0180f24d60b7833bd
Bernd Ahlers <bernd@graylog.com>
2024-11-26 19:01:47 +0100

    Add new command to generate IntelliJ run configurations

--- cmd/idea.go
+++ cmd/idea.go
@@ -1,11 +1,14 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/Graylog2/graylog-project-cli/config"
 	"github.com/Graylog2/graylog-project-cli/idea"
 	"github.com/Graylog2/graylog-project-cli/manifest"
 	p "github.com/Graylog2/graylog-project-cli/project"
+	"github.com/Graylog2/graylog-project-cli/utils"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var ideaCmd = &cobra.Command{
@@ -17,8 +20,9 @@ Commands to help working with the IntelliJ IDE.
 }
 
 var ideaSetupCmd = &cobra.Command{
-	Use:   "setup",
-	Short: "Setup IntelliJ IDEA",
+	Deprecated: `use the "run-config create" command instead`,
+	Use:        "setup",
+	Short:      "Setup IntelliJ IDEA",
 	Long: `
 This command will do the following:
 
@@ -30,8 +34,47 @@ This command will do the following:
 	Run: ideaSetupCommand,
 }
 
+var ideaRunConfigsCmd = &cobra.Command{
+	Use:     "run-configs",
+	Aliases: []string{"rc"},
+	Short:   "Manage IntelliJ IDEA run configurations",
+}
+
+var ideaRunConfigsCreateCmd = &cobra.Command{
+	Use:     "create",
+	Aliases: []string{"c"},
+	Short:   "Create IntelliJ IDEA run configurations",
+	Long: `This command adds default IntelliJ run configurations for Graylog Server,
+Data Node, and the web development server.
+
+The run configurations are created in the $PWD/.idea/runConfigurations/ directory.
+
+Examples:
+    # Create default run configurations
+    graylog-project idea run-config create
+
+    # Create default run configurations and .env files (requires installation of EnvFile plugin in IntelliJ)
+    graylog-project idea run-config create -E
+
+    # Create run configurations for two Server and three Data Node instances
+    graylog-project idea run-config create --instances server=2,data-node=3
+`,
+	RunE: ideaRunConfigCreateCommand,
+}
+
 func init() {
+	ideaRunConfigsCreateCmd.Flags().BoolP("force", "f", false, "Overwrite existing run configurations")
+	ideaRunConfigsCreateCmd.Flags().BoolP("env-file", "E", false, "Use .env files (requires the IntelliJ EnvFile plugin)")
+	ideaRunConfigsCreateCmd.Flags().StringToIntP("instances", "i", idea.DefaultInstanceCounts, "Number of instances - example: server=1,data-node=3")
+	ideaRunConfigsCreateCmd.Flags().String("root-password", idea.DefaultRootPassword, "The root user password")
+	ideaRunConfigsCmd.AddCommand(ideaRunConfigsCreateCmd)
+
+	if err := viper.BindPFlags(ideaRunConfigsCreateCmd.Flags()); err != nil {
+		panic(err)
+	}
+
 	ideaCmd.AddCommand(ideaSetupCmd)
+	ideaCmd.AddCommand(ideaRunConfigsCmd)
 	RootCmd.AddCommand(ideaCmd)
 }
 
@@ -40,4 +83,20 @@ func ideaSetupCommand(cmd *cobra.Command, args []string) {
 	project := p.New(config.Get(), manifestFiles)
 
 	idea.Setup(project)
+}
+
+func ideaRunConfigCreateCommand(cmd *cobra.Command, args []string) error {
+	var cfg idea.RunConfig
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return fmt.Errorf("couldn't parse configuration: %w", err)
+	}
+
+	wd, err := utils.GetCwdE()
+	if err != nil {
+		return err
+	}
+
+	cfg.Workdir = wd
+
+	return idea.CreateRunConfigurations(cfg)
 }
--- go.mod
+++ go.mod
@@ -21,10 +21,12 @@ require (
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
+	github.com/subosito/gotenv v1.6.0
 	github.com/yuin/goldmark v1.7.8
 	golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f
 	golang.org/x/oauth2 v0.24.0
 	golang.org/x/text v0.20.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -46,12 +48,10 @@ require (
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/term v0.26.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 // Workaround for https://github.com/golang/go/issues/30831
